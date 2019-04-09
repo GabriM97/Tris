dcddf98557b4900b208e74c0e1ab348daf863b42
Gabriele <marino.gabri97@gmail.com>
2019-04-09 13:47:02 +0200

    trying to add some animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,23 @@ $(document).ready(function(){
   homePage();
 
 
-})
+});
 
 function homePage(){
-  
+  $("#container").empty();
+  $("#container").addClass("slider").addClass("closed")
 
+  var content =
+  '<h1 id="main-title">Tris Game!</h1> \
+  <div id="form-container"> \
+      <p>Player 1</p> \
+      <input type="text" id="player1-label" placeholder="Name P1"> \
+      <p>Player 2</p> \
+      <input type="text" id="player2-label" placeholder="Name P2"> \
+      <button type="button" name="start">Start</button> \
+      <button type="reset" name="reset">Reset</button> \
+    </div>';
+
+  $("#container").append(content);
+  $("#container").removeClass("closed");
 }
