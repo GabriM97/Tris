74dbbbe9b9b745653ee8668209101f3d2310621a
Gabriele <marino.gabri97@gmail.com>
2019-04-11 00:39:40 +0200

    add tris page with icons and nickname personalization, add some animations. change font family

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
 
+  //showTris("Player1","Player2");
   homePage();
-  addEvents();
+  addEventsOnButtons();
 
 });
 
+//--- INIT PAGE ---
 function homePage(){
   $("#container").empty();
-  $("#container").addClass("slider").addClass("closed");
 
-  var content =
-  '<h1 id="main-title">Tris Game!</h1> \
+  var content ='\
+  <h1 id="main-title">Tris Game!</h1> \
   <div id="form-container"> \
       <p>Player 1</p> \
       <input type="text" id="player1-label" placeholder="Name P1"> \
@@ -24,8 +25,83 @@ function homePage(){
   $("#container").removeClass("closed");
 }
 
-function addEvents(){
+//--- BUTTONS START\RESET EVENT LISTENER ---
+function addEventsOnButtons(){
   $(".btn").on("click", function(){
-    
+    if($(this).text() == "Start"){
+      let player1 = $("#player1-label").val();
+      let player2 = $("#player2-label").val();
+      if(player1 == "" || player2 == ""){
+        player1 = "Player1";
+        player2 = "Player2";
+      }
+      $(this).addClass("fadeOut");
+      showTris(player1, player2);
+    }else if ($(this).text() == "Reset") {
+      resetAll();
+    }
   });
+}
+
+//--- RESET BUTTON ---
+function resetAll(){
+  $("#player1-label").val("");
+  $("#player2-label").val("");
+}
+
+//--- SHOW TRIS \ START BUTTON ---
+function showTris(p1_name, p2_name){
+  $("#container").empty();
+  $("#container").removeClass("fadeOut").removeClass("zoomInDown");
+  $("#container").addClass("fadeIn");
+
+  showTrisData(p1_name, p2_name);
+  showTrisGrid();
+
+  $("#container").removeClass("closed");
+  handleTrisGame();
+}
+
+function showTrisData(p1_name, p2_name){
+  $("#container").append('<div id="top-info-container"></div>');
+
+  var player1_doms = '\
+  <div class="player-info" id="player1">\
+    <div class="player-icon"></div>\
+    <p class="player-name">'+ p1_name +'</p>\
+    <p class="player-points">0</p>\
+  </div>';
+  $("#top-info-container").append(player1_doms);
+
+  var player2_doms = '\
+  <div class="player-info" id="player2">\
+    <div class="player-icon"></div>\
+    <p class="player-name">'+ p2_name +'</p>\
+    <p class="player-points">0</p>\
+  </div>';
+  $("#top-info-container").append(player2_doms);
+
+  var player_turn_doms = '\
+  <div id="turn-container">\
+    <div class="player-info" id="player1">\
+      <span>Turn of</span><p class="player-name">'+ p1_name +'</p>\
+      <div class="player-icon"></div>\
+    </div>\
+  </div>';
+  $("#top-info-container").append(player_turn_doms);
+}
+
+function showTrisGrid(){
+  $("#container").append('<div id="tris-grid"></div>');
+
+  for(var i=1; i<4; i++){
+    $("#tris-grid").append('<div id="r'+i+'c1"></div>');
+    $("#tris-grid").append('<div id="r'+i+'c2"></div>');
+    $("#tris-grid").append('<div id="r'+i+'c3"></div>');
+    $("#tris-grid:last-child::after").css("display","block");
+  }
+}
+
+function handleTrisGame(){
+  // console.log("tris");
 }
